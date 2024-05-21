347e9c8aabccc62028036b9a59febefd2adfb7c8
duskfirst <122384892+duskfirst@users.noreply.github.com>
2024-05-21 23:57:58 +0530

    ui changes in directors message

--- components/about/DirectorsMessage.tsx
+++ components/about/DirectorsMessage.tsx
@@ -1,6 +1,6 @@
-import director from "@/assets/images/faculty/director.jpg";
-import Image from "next/image";
-import Link from "next/link";
+import director from '@/assets/images/faculty/director.jpg';
+import Image from 'next/image';
+import Link from 'next/link';
 interface DirectorsMessageProps {
   heading: string;
   text: string;
@@ -12,15 +12,15 @@ const DirectorsMessage = ({
   directorName,
 }: DirectorsMessageProps) => {
   return (
-    <div className="flex p-4 mt-4 flex-col items-center">
+    <div className="flex p-4 mt-4 flex-col items-center justify-center">
       <span className="text-2xl md:text-4xl mb-8 text-dwd-primary text-center w-full font-semibold">
         Directors Message
       </span>
-      <p className="p-4 flex flex-col items-center md:inline ">
+      <p className="p-4 flex flex-col items-center md:inline content-center">
         <Image
           src={director}
-          alt={"Image of Director"}
-          className="sm:float-left w-3/4 align-middle sm:w-5/12 lg:w-1/3 mb-4 mr-8"
+          alt={'Image of Director'}
+          className="sm:float-left w-3/4 sm:w-5/12 lg:w-1/3 mb-4  md:mr-8"
         />
         <span className="text-dwd-primary font-semibold md:text-lg">
           {heading}
@@ -31,7 +31,7 @@ const DirectorsMessage = ({
         <br />
         <Link
           className="self-start text-dwd-primary hover:cursor-pointer"
-          href={"/faculty"}
+          href={'/faculty'}
         >
           <span className="md:text-lg font-bold">{directorName}</span>
           <br />
--- components/about/OurMission.tsx
+++ components/about/OurMission.tsx
@@ -7,13 +7,13 @@ interface OurMissionProps {
 const OurMission = ({ missions }: OurMissionProps) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 items-center p-8 gap-4 w-full justify-items-center">
-      <span className="col-span-1 md:col-span-3 text-center text-2xl sm:text-3xl md:text-4xl font-bold text-dwd-primary">
+      <span className="col-span-1 md:col-span-3 mb-8 text-center text-2xl sm:text-3xl md:text-4xl font-bold text-dwd-primary">
         Our Mission
       </span>
       {missions?.map((mission) => (
         <div
           key={mission.id}
-          className="bg-dwd-primary p-4 rounded-md text-center md:text-lg md:min-h-56 min-h-40 max-w-96 text-white text-md"
+          className="bg-dwd-primary p-4 rounded-md text-center md:text-lg w-full h-full min-h-44  sm:min-h-28 sm:max-w-96 md:min-h-28  lg:min-h-52 lg:max-w-96 text-white text-md"
         >
           {mission.text}
         </div>
