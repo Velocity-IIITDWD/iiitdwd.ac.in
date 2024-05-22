0fed3f6b3ea4f0a28a12452005d30e032f2e75ab
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2024-05-22 14:27:11 +0530

    404 page + logo redirects to home + ran linter

--- app/clubs/page.tsx
+++ app/clubs/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import React, { FC, useState, useRef } from 'react';
 import { LinkedinIcon, Instagram, Mail } from 'lucide-react';
 import Image from 'next/image';
@@ -33,18 +33,18 @@ const Club: FC = () => {
 
       <div className="bg-dwd-secondary2 p-8 rounded-2xl mb-8 relative">
         <div className="absolute top-4 left-4">
-        <button
-          className={`px-4 py-2 rounded-2xl mr-4 ${showTechnicalClubs ? 'bg-dwd-primary text-white hover:text-sky-600' : 'bg-dwd-secondary1 hover:text-white'}`}
-          onClick={() => setShowTechnicalClubs(true)}
-        >
+          <button
+            className={`px-4 py-2 rounded-2xl mr-4 ${showTechnicalClubs ? 'bg-dwd-primary text-white hover:text-sky-600' : 'bg-dwd-secondary1 hover:text-white'}`}
+            onClick={() => setShowTechnicalClubs(true)}
+          >
           Technical Clubs
-        </button>
-        <button
-          className={`px-4 py-2 rounded-2xl ${!showTechnicalClubs ? 'bg-dwd-primary text-white hover:text-sky-600' : 'bg-dwd-secondary1 hover:text-white'}`}
-          onClick={() => setShowTechnicalClubs(false)}
-        >
+          </button>
+          <button
+            className={`px-4 py-2 rounded-2xl ${!showTechnicalClubs ? 'bg-dwd-primary text-white hover:text-sky-600' : 'bg-dwd-secondary1 hover:text-white'}`}
+            onClick={() => setShowTechnicalClubs(false)}
+          >
           Non Technical Clubs
-        </button>
+          </button>
 
         </div>
 
@@ -104,64 +104,64 @@ const ClubCard: FC<{ clubName: string; aboutText: string; members: { name: strin
   const [showAbout, setShowAbout] = useState(true);
 
   return (
-<div className="relative max-w-sm mx-auto bg-dwd-secondary2 p-4 rounded-md shadow-md mb-8 hover:border-dwd-primary border-2 border-transparent">
-  <div className="absolute -top-8 left-1/2 transform -translate-x-1/2">
-  <Image src={imagePath} alt={`${clubName} logo`} width={110} height={110} className="rounded-full border-4 border-gray-200" />
+    <div className="relative max-w-sm mx-auto bg-dwd-secondary2 p-4 rounded-md shadow-md mb-8 hover:border-dwd-primary border-2 border-transparent">
+      <div className="absolute -top-8 left-1/2 transform -translate-x-1/2">
+        <Image src={imagePath} alt={`${clubName} logo`} width={110} height={110} className="rounded-full border-4 border-gray-200" />
 
-  </div>
-  <br />
-  <div className="pt-12">
-    <h2 className="text-center text-2xl font-bold mb-4">{clubName}</h2>
-    <div className="flex justify-center mb-4">
-      <button
-        className={`px-4 py-2 cursor-pointer rounded-md mr-2 ${showAbout ? 'bg-dwd-primary hover:text-sky-600 text-white' : 'bg-dwd-secondary1 hover:text-white'}`}
-        onClick={() => setShowAbout(true)}
-      >
-        ABOUT
-      </button>
-      <button
-        className={`px-4 py-2 cursor-pointer hover:text-sky-600 rounded-md ${!showAbout ? 'bg-dwd-primary hover:text-sky-600 text-white' : 'bg-dwd-secondary1 hover:text-white'}`}
-        onClick={() => setShowAbout(false)}
-      >
-        MEMBERS
-      </button>
-    </div>
-    {showAbout ? (
-      <div className="text-center mb-4">{aboutText}</div>
-    ) : (
-      <div className="text-center mb-4">
-        <table className="min-w-full bg-dwd-secondary2">
-          <thead>
-            <tr>
-              <th className="py-2">Name</th>
-              <th className="py-2">Position</th>
-            </tr>
-          </thead>
-          <tbody>
-            {members.map((member, index) => (
-              <tr key={index}>
-                <td className="border px-4 py-2">{member.name}</td>
-                <td className="border px-4 py-2">{member.position}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
       </div>
-    )}
-    <br />
-    <div className="flex justify-center space-x-4">
-      <a href="https://www.instagram.com" className="flex gap-2">
-        <Instagram size="2rem" />
-      </a>
-      <a href="https://www.linkedin.com/in/iiitdharwad/" className="flex gap-2">
-        <LinkedinIcon size="2rem" />
-      </a>
-      <a href="https://www.gmail.com" className="flex gap-2">
-        <Mail size="2rem" />
-      </a>
+      <br />
+      <div className="pt-12">
+        <h2 className="text-center text-2xl font-bold mb-4">{clubName}</h2>
+        <div className="flex justify-center mb-4">
+          <button
+            className={`px-4 py-2 cursor-pointer rounded-md mr-2 ${showAbout ? 'bg-dwd-primary hover:text-sky-600 text-white' : 'bg-dwd-secondary1 hover:text-white'}`}
+            onClick={() => setShowAbout(true)}
+          >
+        ABOUT
+          </button>
+          <button
+            className={`px-4 py-2 cursor-pointer hover:text-sky-600 rounded-md ${!showAbout ? 'bg-dwd-primary hover:text-sky-600 text-white' : 'bg-dwd-secondary1 hover:text-white'}`}
+            onClick={() => setShowAbout(false)}
+          >
+        MEMBERS
+          </button>
+        </div>
+        {showAbout ? (
+          <div className="text-center mb-4">{aboutText}</div>
+        ) : (
+          <div className="text-center mb-4">
+            <table className="min-w-full bg-dwd-secondary2">
+              <thead>
+                <tr>
+                  <th className="py-2">Name</th>
+                  <th className="py-2">Position</th>
+                </tr>
+              </thead>
+              <tbody>
+                {members.map((member, index) => (
+                  <tr key={index}>
+                    <td className="border px-4 py-2">{member.name}</td>
+                    <td className="border px-4 py-2">{member.position}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+        <br />
+        <div className="flex justify-center space-x-4">
+          <a href="https://www.instagram.com" className="flex gap-2">
+            <Instagram size="2rem" />
+          </a>
+          <a href="https://www.linkedin.com/in/iiitdharwad/" className="flex gap-2">
+            <LinkedinIcon size="2rem" />
+          </a>
+          <a href="https://www.gmail.com" className="flex gap-2">
+            <Mail size="2rem" />
+          </a>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
 
   );
 };
--- lib/utils.ts
+++ lib/utils.ts
@@ -2,26 +2,26 @@ import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs))
 }
 
 export function toDateString(date: number) {
-    const dateObj = new Date(date);
-    return dateObj.toLocaleString('default', {
-        day: '2-digit',
-        month: 'short',
-        year: 'numeric',
-    });
+  const dateObj = new Date(date);
+  return dateObj.toLocaleString('default', {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric',
+  });
 }
 
 export function toDateTimeString(date: number) {
-    const dateObj = new Date(date);
-    return dateObj.toLocaleString('default', {
-        day: '2-digit',
-        month: 'short',
-        year: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false,
-    });
+  const dateObj = new Date(date);
+  return dateObj.toLocaleString('default', {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  });
 }
