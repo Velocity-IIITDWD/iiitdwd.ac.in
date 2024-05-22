f897b692ab88b8444cb4ec562458bb8364626299
Yogeshgouda Patil <136431159+phantomsoldierking@users.noreply.github.com>
2024-05-22 10:39:59 +0530

    added_key-index-for-areaodinterest_object
    to use map area of interest did not have a key  it is now added

--- data/faculty_profile.ts
+++ data/faculty_profile.ts
@@ -1,138 +1,169 @@
-
 interface ProfileProp {
-    id: string,
-    content: {head: {
-        name: string,
-        profile_pdf: string
-    },
+  id: string;
+  content: {
+    head: {
+      name: string;
+      profile_pdf: string;
+    };
 
     card: {
-        photo: string,
-        designation: string,
-        department: string,
-        mail_id: string,
-        cabin_number: string
-    },
+      photo: string;
+      designation: string;
+      department: string;
+      mail_id: string;
+      cabin_number: string;
+    };
 
-    body:{
-        profile_text: string,
-        interest_areas: string[]
-    }}
+    body: {
+      profile_text: string;
+      interest_areas: { id: number; area: string }[];
+    };
+  };
+}
+[];
 
-}[];
-
-let ProfileProps:Array<ProfileProp> = 
-    [{
-        id: '1',
-        content: {head: {
-            name: 'Dr. Abdul Wahid',
-            profile_pdf: '--provide path string'
-        },
-        card: {
-            photo: '/brand/logo-square-light.png',
-            designation: 'Assistant professor',
-            department: 'Computer Science & Engineering',
-            mail_id: 'abdul.wahid@iiitdwd.ac.in',
-            cabin_number: '?????'
-        },
-        body: {
-            profile_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-            interest_areas: ["Artificial Intelligence", "Evolutionary Computing", "Reinforcement Learning", "Multi-Agent Systems"]
-        }}
+let ProfileProps: Array<ProfileProp> = [
+  {
+    id: '1',
+    content: {
+      head: {
+        name: 'Dr. Abdul Wahid',
+        profile_pdf: '--provide path string',
+      },
+      card: {
+        photo: '/brand/logo-square-light.png',
+        designation: 'Assistant professor',
+        department: 'Computer Science & Engineering',
+        mail_id: 'abdul.wahid@iiitdwd.ac.in',
+        cabin_number: '?????',
+      },
+      body: {
+        profile_text:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        interest_areas: [
+          { id: 1, area: 'Artificial Intelligence' },
+          { id: 2, area: 'Evolutionary Computing' },
+          { id: 3, area: 'Reinforcement Learning' },
+          { id: 4, area: 'Multi-Agent Systems' },
+        ],
+      },
     },
-    {
-        id: '2',
-        content: {head: {
-            name: 'Dr. ANAND P. BARANGI',
-            profile_pdf: '--provide path string'
-        },
-        card: {
-            photo: '/brand/logo-square-light.png',
-            designation: 'Assistant professor',
-            department: 'Humanities and Science',
-            mail_id: 'anandbarangi@iiitdwd.ac.in',
-            cabin_number: '?????'
-        },
-        body: {
-            profile_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-            interest_areas: ["Graph Theory"]
-        }}
+  },
+  {
+    id: '2',
+    content: {
+      head: {
+        name: 'Dr. ANAND P. BARANGI',
+        profile_pdf: '--provide path string',
+      },
+      card: {
+        photo: '/brand/logo-square-light.png',
+        designation: 'Assistant professor',
+        department: 'Humanities and Science',
+        mail_id: 'anandbarangi@iiitdwd.ac.in',
+        cabin_number: '?????',
+      },
+      body: {
+        profile_text:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        interest_areas: [{ id: 1, area: 'Graph Theory' }],
+      },
     },
-    {
-        id: '3',
-        content: {head: {
-            name: 'Dr. Animesh Chaturvedi',
-            profile_pdf: '--provide path string'
-        },
-        card: {
-            photo: '/brand/logo-square-light.png',
-            designation: 'Assistant professor',
-            department: 'Data Science and Intelligent Systems',
-            mail_id: 'animesh@iiitdwd.ac.in',
-            cabin_number: '?????'
-        },
-        body: {
-            profile_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-            interest_areas: ["Data Science and AI", "Systems Engineering", " SOA and Cloud Computing", "Big Data Analytics"]
-        }}
+  },
+  {
+    id: '3',
+    content: {
+      head: {
+        name: 'Dr. Animesh Chaturvedi',
+        profile_pdf: '--provide path string',
+      },
+      card: {
+        photo: '/brand/logo-square-light.png',
+        designation: 'Assistant professor',
+        department: 'Data Science and Intelligent Systems',
+        mail_id: 'animesh@iiitdwd.ac.in',
+        cabin_number: '?????',
+      },
+      body: {
+        profile_text:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        interest_areas: [
+          { id: 1, area: 'Data Science and AI' },
+          { id: 2, area: 'Systems Engineering' },
+          { id: 3, area: ' SOA and Cloud Computing' },
+          { id: 4, area: 'Big Data Analytics' },
+        ],
+      },
     },
-    {
-        id: '4',
-        content: {head: {
-            name: 'Dr. Animesh Roy',
-            profile_pdf: '--provide path string'
-        },
-        card: {
-            photo: '/brand/logo-square-light.png',
-            designation: 'Assistant professor',
-            department: 'Computer Science & Engineering',
-            mail_id: 'animeshroy@iiitdwd.ac.in',
-            cabin_number: '?????'
-        },
-        body: {
-            profile_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-            interest_areas: [" Delay tolerant networks", "Network optimization","Algorithms"]
-        }}
+  },
+  {
+    id: '4',
+    content: {
+      head: {
+        name: 'Dr. Animesh Roy',
+        profile_pdf: '--provide path string',
+      },
+      card: {
+        photo: '/brand/logo-square-light.png',
+        designation: 'Assistant professor',
+        department: 'Computer Science & Engineering',
+        mail_id: 'animeshroy@iiitdwd.ac.in',
+        cabin_number: '?????',
+      },
+      body: {
+        profile_text:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        interest_areas: [
+          { id: 1, area: 'Delay tolerant networks' },
+          { id: 2, area: 'Network optimization' },
+          { id: 3, area: 'Algorithms' },
+        ],
+      },
     },
-    {
-        id: '5',
-        content: {head: {
-            name: 'Dr. Anushree kini',
-            profile_pdf: '--provide path string'
-        },
-        card: {
-            photo: '/brand/logo-square-light.png',
-            designation: 'Humanities & Science',
-            department: 'Computer Science & Engineering',
-            mail_id: 'anushree@iiitdwd.ac.in',
-            cabin_number: '?????'
-        },
-        body: {
-            profile_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-            interest_areas: []
-        }}
+  },
+  {
+    id: '5',
+    content: {
+      head: {
+        name: 'Dr. Anushree kini',
+        profile_pdf: '--provide path string',
+      },
+      card: {
+        photo: '/brand/logo-square-light.png',
+        designation: 'Humanities & Science',
+        department: 'Computer Science & Engineering',
+        mail_id: 'anushree@iiitdwd.ac.in',
+        cabin_number: '?????',
+      },
+      body: {
+        profile_text:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        interest_areas: [],
+      },
     },
-    {
-        id: '6',
-        content: {head: {
-            name: 'Dr. Anushree kini',
-            profile_pdf: '--provide path string'
-        },
-        card: {
-            photo: '/brand/logo-square-light.png',
-            designation: 'Humanities & Science',
-            department: 'Computer Science & Engineering',
-            mail_id: 'anushree@iiitdwd.ac.in',
-            cabin_number: '?????'
-        },
-        body: {
-            profile_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-            interest_areas: []
-        }}
-    }
-    
-]
-
+  },
+  {
+    id: '6',
+    content: {
+      head: {
+        name: 'Dr. Anushree kini',
+        profile_pdf: '--provide path string',
+      },
+      card: {
+        photo: '/brand/logo-square-light.png',
+        designation: 'Humanities & Science',
+        department: 'Computer Science & Engineering',
+        mail_id: 'anushree@iiitdwd.ac.in',
+        cabin_number: '?????',
+      },
+      body: {
+        profile_text:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        interest_areas: [],
+      },
+    },
+  },
+];
 
 //ProfileProps.sort();
 
