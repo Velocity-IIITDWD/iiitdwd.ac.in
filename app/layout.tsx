9812f342a8988384ec737ce0ce3583663871291c
Bhagya <bhagyavishwakarma88@gmail.com>
2024-05-22 11:16:07 +0530

    career

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import { Inter } from 'next/font/google';
 
+import "./globals.css";
+
+
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
