2bbe6b96e9f3cbe9a6f53272f3944f3522a96ffd
boxlesscat <boxlesscat@gmail.com>
2024-05-27 17:05:51 +0530

    add pages for cgc committee and student coordinators team

---
+++ data/profile/cgc/student_coordinators.ts
@@ -0,0 +1,97 @@
+import { ProfileGroup } from '@/types/profile';
+
+export default [
+  {
+    title: '',
+    profiles: [
+      {
+        content: ['Lead Coordinator'],
+        imageURL:
+          'https://iiitdwd.ac.in/commitee_page/student_coordinators/IMG-20230709-WA0024%20-%20OM%20MORENDHA%20IIIT%20Dharwad.jpg',
+        title: 'Om Morendha',
+      },
+      {
+        content: ['Lead Coordinator'],
+        imageURL:
+          'https://iiitdwd.ac.in/commitee_page/student_coordinators/Shreyas_Bulbule.jpeg',
+        title: 'Shreyas Bulbule',
+      },
+      {
+        content: ['Lead Coordinator'],
+        imageURL:
+          'https://iiitdwd.ac.in/commitee_page/student_coordinators/Screenshot_20230721-204026_Photos%20-%20GAURANG%20SUBHASH%20BELEKAR%20IIIT%20Dharwad.jpg',
+        title: 'Gaurang Belekar',
+      },
+    ],
+  },
+  {
+    title: '',
+    profiles: [
+      {
+        content: ['Coordinator'],
+        imageURL:
+          'https://iiitdwd.ac.in/commitee_page/student_coordinators/Snapchat-1219141046%20-%20SIDDHANT%20DIXIT%20IIIT%20Dharwad.jpg',
+        title: 'Siddhant Dixit',
+      },
+      {
+        content: ['Coordinator'],
+        imageURL:
+          'https://iiitdwd.ac.in/commitee_page/student_coordinators/Nikhil%20B.jpeg',
+        title: 'Bhashapangu Nikhil',
+      },
+      {
+        content: ['Coordinator'],
+        imageURL:
+          'https://iiitdwd.ac.in/commitee_page/student_coordinators/IMG_20230508_222905_671%20-%20HARSH%20RAWAT%20IIIT%20Dharwad.jpg',
+        title: 'Harsh Rawat',
+      },
+      {
+        content: ['Coordinator'],
+        imageURL:
+          'https://iiitdwd.ac.in/commitee_page/student_coordinators/IMG_20230817_194502%20-%20RISHIKA%20KUMARI%20IIIT%20Dharwad.jpg',
+        title: 'Rishika Kumari',
+      },
+      {
+        content: ['Coordinator'],
+        imageURL:
+          'https://iiitdwd.ac.in/commitee_page/student_coordinators/Screenshot_20230806-195240_Photos%20-%20GARIMELLA%20AMRUTHAVALLI%20IIIT%20Dharwad.jpg',
+        title: 'Garimella Amruthavalli',
+      },
+      {
+        content: ['Coordinator'],
+        imageURL:
+          'https://iiitdwd.ac.in/commitee_page/student_coordinators/Screenshot%202023-08-09%20at%203.34.18%20PM%20-%20GAURAV%20AGARWAL%20IIIT%20Dharwad.png',
+        title: 'Gaurav Agarwal',
+      },
+      {
+        content: ['Coordinator'],
+        imageURL:
+          'https://iiitdwd.ac.in/commitee_page/student_coordinators/RAJNISH-20BEC035%20-%20RAJNISH%20KUMAR%20IIIT%20Dharwad.jpg',
+        title: 'Rajnish Kumar',
+      },
+      {
+        content: ['Coordinator'],
+        imageURL:
+          'https://iiitdwd.ac.in/commitee_page/student_coordinators/IMG-20230724-WA0001%20-%20MOHIT%20SAINI%20IIIT%20Dharwad.jpg',
+        title: 'Mohit Saini',
+      },
+      {
+        content: ['Coordinator'],
+        imageURL:
+          'https://iiitdwd.ac.in/commitee_page/student_coordinators/IMG_20231004_133647%20-%20B%20SRI%20VENKATA%20SAI%20TARUN%20IIIT%20Dharwad.jpg',
+        title: 'B Tarun',
+      },
+      {
+        content: ['Coordinator'],
+        imageURL:
+          'https://iiitdwd.ac.in/commitee_page/student_coordinators/poornima_pic%20-%20POORNIMA%20IIIT%20Dharwad.jpeg',
+        title: 'Poornima',
+      },
+      {
+        content: ['Coordinator'],
+        imageURL: '',
+        title: 'Kanishk Bajpai',
+      },
+    ],
+  },
+] as ProfileGroup[];
---
+++ data/profile/committee/cgc.ts
@@ -0,0 +1,74 @@
+import { ProfileGroup } from '@/types/profile';
+
+export default [
+  {
+    title: '',
+    profiles: [
+      {
+        content: ['Chairman'],
+        imageURL: 'https://www.iiitdwd.ac.in/img/faculty/Prakash.jpg',
+        title: 'Dr. Prakash Pawar',
+      },
+      {
+        content: ['Member Secretary'],
+        imageURL: 'https://iiitdwd.ac.in/assets/img/ujwal.jpeg',
+        title: 'Mr. Ujwal Doddamani',
+      },
+    ],
+  },
+  {
+    title: '',
+    profiles: [
+      {
+        content: ['Ex-officio Member'],
+        imageURL: 'https://iiitdwd.ac.in/img/faculty/deepak.jpeg',
+        title: 'Dr. Deepak KT',
+      },
+      {
+        content: ['Ex-officio Member'],
+        imageURL: 'https://iiitdwd.ac.in/img/faculty/pavan.jpg',
+        title: 'Dr. Pavan Kumar',
+      },
+      {
+        content: ['Ex-officio Member'],
+        imageURL: 'https://iiitdwd.ac.in/img/faculty/Sunil.jpeg',
+        title: 'Dr. Sunil Saumya',
+      },
+      {
+        content: ['Ex-officio Member'],
+        imageURL: 'https://iiitdwd.ac.in/img/faculty/aswath.jpeg',
+        title: 'Dr. Ashwath Babu H',
+      },
+    ],
+  },
+  {
+    title: '',
+    profiles: [
+      {
+        content: ['Member'],
+        imageURL: 'https://iiitdwd.ac.in/img/faculty/Animesh.jpg',
+        title: 'Dr. Animesh Chaturvedi',
+      },
+      {
+        content: ['Member'],
+        imageURL: 'https://www.iiitdwd.ac.in/img/faculty/yelmewad.jpg',
+        title: 'Dr. Pramod Yelmewad',
+      },
+      {
+        content: ['Member'],
+        imageURL: 'https://iiitdwd.ac.in/img/faculty/vivekraj.jpg',
+        title: 'Dr. Vivek Raj',
+      },
+      {
+        content: ['Member'],
+        imageURL: 'https://iiitdwd.ac.in/img/faculty/Prabhu_Prasad.png',
+        title: 'Dr. Prabhu Prasad BM',
+      },
+      {
+        content: ['Member'],
+        imageURL: 'https://iiitdwd.ac.in/img/faculty/Anushree.jpg',
+        title: 'Dr. Anushree Kini',
+      },
+    ],
+  },
+] as ProfileGroup[];
