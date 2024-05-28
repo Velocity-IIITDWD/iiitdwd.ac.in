f4adbd9376da8cca1f6b19a5fe8c8f671e220aff
Harsh Soni <52278293+harshsoni-harsh@users.noreply.github.com>
2024-05-28 18:10:21 +0530

    Added WIP in assets

--- app/admissions/phd/page.tsx
+++ app/admissions/phd/page.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image';
+import WIP from '@/assets/work_in_progress.png'
 
 export default function Page() {
   return <div className="flex justify-center items-center grow">
-    <Image src="/images/work_in_progress.png" width={240} height={240} alt="work in progress" />
+    <Image src={WIP} width={240} height={240} alt="work in progress" />
   </div>
 }
