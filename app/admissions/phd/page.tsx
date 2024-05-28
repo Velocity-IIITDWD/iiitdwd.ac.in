81bd3b360334d5fa291ea02c13277745af68ba00
Harsh Soni <52278293+harshsoni-harsh@users.noreply.github.com>
2024-05-28 17:01:06 +0530

    Added mtech page

---
+++ app/admissions/phd/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Page() {
+  return <div className="flex justify-center items-center grow">
+    <Image src="/images/work_in_progress.png" width={240} height={240} alt="work in progress" />
+  </div>
+}
