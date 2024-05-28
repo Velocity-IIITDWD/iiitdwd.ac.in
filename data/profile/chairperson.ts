af1b6ea1d69a4accf12db6412c64eb1ec2e7e5db
boxlesscat <boxlesscat@gmail.com>
2024-05-28 20:39:06 +0530

    move former chairperson page and add chairperson page

---
+++ data/profile/chairperson.ts
@@ -0,0 +1,14 @@
+import { ProfileGroup } from '@/types/profile';
+
+export default [
+  {
+    title: 'Officiating Chairperson',
+    profiles: [
+      {
+        content: ['Officiating Chairperson & Director, IIIT Dharwad'],
+        imageURL: '/images/s_r_mahadeva_prasanna.JPG',
+        title: 'Prof. S. R. Mahadeva Prasanna',
+      },
+    ],
+  },
+] as ProfileGroup[];
