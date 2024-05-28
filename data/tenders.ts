1b415697b172ea32fe69e15c5e070244fea7f47c
boxlesscat <boxlesscat@gmail.com>
2024-05-28 17:41:21 +0530

    add timezones to tenders data

--- data/tenders.ts
+++ data/tenders.ts
@@ -14,7 +14,7 @@ export const tenders: Tender[] = [
     ],
     link: 'https://iiitdwd.ac.in/docs/Tenders/EXPRESSIONOFINTEREST.pdf',
     publishDate: '2024-05-16',
-    submissionDeadline: '2024-05-27T17:00',
+    submissionDeadline: '2024-05-27T17:00+05:30',
     title: 'Expression of Interest (Hostel Accommodation for Boys)',
     updatedAt: 49,
   },
@@ -34,7 +34,7 @@ export const tenders: Tender[] = [
     ],
     link: 'https://iiitdwd.ac.in/docs/Tenders/G%20-%20block-Aluminium-partition-work-final-07032024.pdf',
     publishDate: '2024-03-07',
-    submissionDeadline: '2024-04-01T14:00',
+    submissionDeadline: '2024-04-01T14:00+05:30',
     title:
       'Providing Partition Wall, Doors and other civil works at g-block IIIT Dharwad',
     updatedAt: 49,
@@ -45,7 +45,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/GeM-Bidding-6107519.pdf',
     publishDate: '2024-02-22',
-    submissionDeadline: '2024-03-18T15:00',
+    submissionDeadline: '2024-03-18T15:00+05:30',
     title: 'Providing, Supplying and Installing Furnitures at IIIT Dharwad',
     updatedAt: 46,
   },
@@ -55,7 +55,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/NIT_CCTV_Digital_Surveillance_System_v1.pdf',
     publishDate: '2023-12-26',
-    submissionDeadline: '2024-01-16T15:00',
+    submissionDeadline: '2024-01-16T15:00+05:30',
     title:
       'Supplying, Installation, Testing and Commissioning Digital Surveillance System [DSS] Unit for existing CCTV Setup at Data Centre of IIIT Dharwad',
     updatedAt: 45,
@@ -75,7 +75,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_54.pdf',
     publishDate: '2022-06-03',
-    submissionDeadline: '2022-06-28T15:00',
+    submissionDeadline: '2022-06-28T15:00+05:30',
     title:
       'Providing, Supplying, Installing, Testing, Deployment and Commissioning of Internet leased Line at IIIT Dharwad',
     updatedAt: 44,
@@ -91,7 +91,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_53.pdf',
     publishDate: '2022-05-19',
-    submissionDeadline: '2022-06-17T15:00',
+    submissionDeadline: '2022-06-17T15:00+05:30',
     title:
       'Supplying, Installing, Testing, Deployment and Commissioning of Firewall at IIIT Dharwad',
     updatedAt: 43,
@@ -102,7 +102,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_52.pdf',
     publishDate: '2022-04-26',
-    submissionDeadline: '2022-05-25T15:00',
+    submissionDeadline: '2022-05-25T15:00+05:30',
     title:
       'Providing, Supplying, Installing, Testing, Deployment and Commissioning of Desktop Computers at IIIT Dharwad',
     updatedAt: 40,
@@ -118,7 +118,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_52.pdf',
     publishDate: '2022-04-02',
-    submissionDeadline: '2022-05-02T17:00',
+    submissionDeadline: '2022-05-02T17:00+05:30',
     title:
       'Running Shops ( Saloon / Pharmacy & General Stores) at IIIT Dharwad Campus',
     updatedAt: 39,
@@ -134,7 +134,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_49.pdf',
     publishDate: '2022-04-02',
-    submissionDeadline: '2022-05-02T17:00',
+    submissionDeadline: '2022-05-02T17:00+05:30',
     title: 'Running Canteen at IIIT Dharwad h -block" at IIIT Dharwad Campus',
     updatedAt: 38,
   },
@@ -153,7 +153,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_43.pdf',
     publishDate: '2022-01-07',
-    submissionDeadline: '2022-02-07T15:00',
+    submissionDeadline: '2022-02-07T15:00+05:30',
     title:
       'Providing, Supplying, Installing, Testing, Deployment and Commissioning of GPU based Server at IIIT Dharwad',
     updatedAt: 35,
@@ -173,7 +173,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_42.pdf',
     publishDate: '2021-12-30',
-    submissionDeadline: '2022-02-08T15:00',
+    submissionDeadline: '2022-02-08T15:00+05:30',
     title:
       'Providing, Supplying, Installing and Commissioning of Gym/ Fitness Equipments at IIIT Dharwad Campus',
     updatedAt: 33,
@@ -184,7 +184,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_41.pdf',
     publishDate: '2021-08-05',
-    submissionDeadline: '2021-08-30T15:00',
+    submissionDeadline: '2021-08-30T15:00+05:30',
     title:
       'Providing, Supplying, Installing, Testing, Deployment and Commissioning of GPU based Server at IIIT Dharwad',
     updatedAt: 29,
@@ -195,7 +195,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_40.pdf',
     publishDate: '2021-08-03',
-    submissionDeadline: '2021-08-30T15:00',
+    submissionDeadline: '2021-08-30T15:00+05:30',
     title:
       'Providing, Supplying and Installing Hostel Furniture at IIIT Dharwad New Campus',
     updatedAt: 28,
@@ -206,7 +206,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_39.pdf',
     publishDate: '2021-08-03',
-    submissionDeadline: '2021-08-19T15:00',
+    submissionDeadline: '2021-08-19T15:00+05:30',
     title:
       'Providing, Supplying and carrying out electrical works in Electronics, Computer Science lab and Hostels at IIIT Dharwad',
     updatedAt: 27,
@@ -217,7 +217,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_38.pdf',
     publishDate: '2021-02-18',
-    submissionDeadline: '2021-03-16T15:00',
+    submissionDeadline: '2021-03-16T15:00+05:30',
     title:
       'Providing, Supplying, Installing, Testing, Deployment and Commissioning of GPU based Server at IIIT Dharwad',
     updatedAt: 26,
@@ -228,7 +228,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_37.pdf',
     publishDate: '2021-02-08',
-    submissionDeadline: '2021-02-23T15:00',
+    submissionDeadline: '2021-02-23T15:00+05:30',
     title:
       'Supply, Installation, Testing of Graphic Processing Unit (GPU) at IIIT Dharwad new campus',
     updatedAt: 25,
@@ -244,7 +244,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_33.pdf',
     publishDate: '2020-12-19',
-    submissionDeadline: '2020-12-28T15:00',
+    submissionDeadline: '2020-12-28T15:00+05:30',
     title:
       'Providing, Supplying, Installing, Testing, Deployment and Commissioning of CCTV at IIIT Dharwad campus',
     updatedAt: 24,
@@ -260,7 +260,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_34.pdf',
     publishDate: '2020-12-19',
-    submissionDeadline: '2021-01-04T15:00',
+    submissionDeadline: '2021-01-04T15:00+05:30',
     title:
       'Providing, Supplying, Installing, Testing and Commissioning of Home Appliances (TVs & Dryers) at IIIT Dharwad campus',
     updatedAt: 22,
@@ -271,7 +271,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_32.pdf',
     publishDate: '2020-11-25',
-    submissionDeadline: '2020-12-15T15:00',
+    submissionDeadline: '2020-12-15T15:00+05:30',
     title:
       'Providing, Supplying, Installing, Testing, Deployment and Commissioning of servers at IIIT Dharwad campus',
     updatedAt: 20,
@@ -287,7 +287,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_27.pdf',
     publishDate: '2020-09-29',
-    submissionDeadline: '2020-11-20T17:00',
+    submissionDeadline: '2020-11-20T17:00+05:30',
     title:
       'Providing 20 to 25 seater bus with driver to IIIT Dharwad, Tasdasinakoppa on rental basis',
     updatedAt: 19,
@@ -298,7 +298,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_30.pdf',
     publishDate: '2020-11-10',
-    submissionDeadline: '2020-11-25T10:00',
+    submissionDeadline: '2020-11-25T10:00+05:30',
     title:
       'Procurement of Automatic Fully Mechanised Organic Waste Machine for Compost',
     updatedAt: 18,
@@ -314,7 +314,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_24.pdf',
     publishDate: '2020-09-05',
-    submissionDeadline: '2020-09-28T15:00',
+    submissionDeadline: '2020-09-28T15:00+05:30',
     title:
       'Providing and Supplying of Security and Housekeeping services to IIIT Dharwad, Tadasinakoppa, Dharwad',
     updatedAt: 17,
@@ -330,7 +330,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_23.pdf',
     publishDate: '2020-09-03',
-    submissionDeadline: '2020-09-24T15:00',
+    submissionDeadline: '2020-09-24T15:00+05:30',
     title: 'Providing and Supplying Catering Services at IIIT Dharwad',
     updatedAt: 16,
   },
@@ -340,7 +340,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: '',
     publishDate: '2020-10-01',
-    submissionDeadline: '2020-10-16T17:00',
+    submissionDeadline: '2020-10-16T17:00+05:30',
     title: 'Washing Machines (GEM/2020/B/816520)',
     updatedAt: 14,
   },
@@ -350,7 +350,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: '',
     publishDate: '2020-09-24',
-    submissionDeadline: '2020-10-09T17:00',
+    submissionDeadline: '2020-10-09T17:00+05:30',
     title: 'Stainless Steel Dining Table With Fixed Chair (GEM/2020/B/806449)',
     updatedAt: 13,
   },
@@ -365,7 +365,7 @@ export const tenders: Tender[] = [
     ],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_23.pdf',
     publishDate: '2020-09-03',
-    submissionDeadline: '2020-09-24T17:00',
+    submissionDeadline: '2020-09-24T17:00+05:30',
     title: 'Providing and Supplying Catering Services at IIIT Dharwad',
     updatedAt: 12,
   },
@@ -375,7 +375,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_25.pdf',
     publishDate: '2020-09-12',
-    submissionDeadline: '2020-09-26T17:00',
+    submissionDeadline: '2020-09-26T17:00+05:30',
     title:
       'Providing 30 to 34 - seater bus with driver to IIIT Dharwad, Tasdasinakoppa on rental basis',
     updatedAt: 11,
@@ -396,7 +396,7 @@ export const tenders: Tender[] = [
     ],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_20.pdf',
     publishDate: '2020-07-07',
-    submissionDeadline: '2020-08-03T15:00',
+    submissionDeadline: '2020-08-03T15:00+05:30',
     title:
       'Supplying, Installing, Testing & Commissioning and Deployment for providing Networking & Wi-Fi Infrastructure facilities at IIIT Dharwad new campus',
     updatedAt: 8,
@@ -412,7 +412,7 @@ export const tenders: Tender[] = [
     ],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_17.pdf',
     publishDate: '2020-02-18',
-    submissionDeadline: '2020-03-16T15:00',
+    submissionDeadline: '2020-03-16T15:00+05:30',
     title:
       'Supply and Installation of Steel Furniture at IIIT Dharwad New Campus',
     updatedAt: 5,
@@ -428,7 +428,7 @@ export const tenders: Tender[] = [
     ],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_16.pdf',
     publishDate: '2020-02-14',
-    submissionDeadline: '2020-03-12T15:00',
+    submissionDeadline: '2020-03-12T15:00+05:30',
     title:
       'Supply and Installation of Office and Class Room furniture at IIIT Dharwad New Campus',
     updatedAt: 5,
@@ -439,7 +439,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_15.pdf',
     publishDate: '2020-01-10',
-    submissionDeadline: '2020-01-31T15:00',
+    submissionDeadline: '2020-01-31T15:00+05:30',
     title:
       'Supply, Installation, Demonstration, and Training for RFID based Library Automation Setup at IIIT Dharwad',
     updatedAt: 1,
@@ -450,7 +450,7 @@ export const tenders: Tender[] = [
     documents: [],
     link: 'https://iiitdwd.ac.in/docs/Tenders/Tender_14.pdf',
     publishDate: '2019-10-17',
-    submissionDeadline: '2019-11-18T12:00',
+    submissionDeadline: '2019-11-18T12:00+05:30',
     title:
       'Supply, Installation, Demonstration and Training for Audio Video Studio Setup at IIIT Dharwad',
     updatedAt: 0,
