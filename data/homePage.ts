dc107ea18313408ac3b15f5702017b0b46bc5bef
AyushSingh916 <ayushsingh916924@gmail.com>
2024-05-28 23:36:15 +0530

    adding images

--- data/homePage.ts
+++ data/homePage.ts
@@ -40,19 +40,19 @@ export const images: MainCarouselImage[] = [
 
 export const ProgramCardDetails: ProgramCards[] = [
   {
-    image: '/main_building.JPG',
+    image: '/cse.jpg',
     link: '/academics/departments/cse',
     Header1: 'Computer Science and Engineering',
     Header2: 'B-Tech (CSE)',
   },
   {
-    image: '/main_building.JPG',
+    image: '/mitra.jpg',
     link: '/academics/departments/dsai',
     Header1: 'Data Science and Artificial Intelligence',
     Header2: 'B-Tech (DSAI)',
   },
   {
-    image: '/main_building.JPG',
+    image: '/drone.jpg',
     link: '/academics/departments/ece',
     Header1: 'Electronics and Communications Engineering',
     Header2: 'B-Tech (ECE)',
