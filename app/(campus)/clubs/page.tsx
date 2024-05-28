902e7d582f758ce1411400d5ff41f3ff9e4bdb95
Nikhil Karthik <nikhilkarthik241103@gmail.com>
2024-05-29 01:35:39 +0530

    clubs cards fixed

--- app/(campus)/clubs/page.tsx
+++ app/(campus)/clubs/page.tsx
@@ -10,15 +10,15 @@ const Club: FC = () => {
 
   const refs: Record<ClubName, React.RefObject<HTMLDivElement>> = {
     'Return 0': useRef<HTMLDivElement>(null),
-    'BlocSoc': useRef<HTMLDivElement>(null),
-    'Velocity': useRef<HTMLDivElement>(null),
+    BlocSoc: useRef<HTMLDivElement>(null),
+    Velocity: useRef<HTMLDivElement>(null),
     'GDSC IIIT Dharwad': useRef<HTMLDivElement>(null),
     'E cell': useRef<HTMLDivElement>(null),
-    'Inquizitive': useRef<HTMLDivElement>(null),
-    'Iris': useRef<HTMLDivElement>(null),
+    Inquizitive: useRef<HTMLDivElement>(null),
+    Iris: useRef<HTMLDivElement>(null),
     'DSAI Society': useRef<HTMLDivElement>(null),
     'Quantum Computing Club': useRef<HTMLDivElement>(null),
-    'Zeitgeist': useRef<HTMLDivElement>(null),
+    Zeitgeist: useRef<HTMLDivElement>(null),
     'Music Club': useRef<HTMLDivElement>(null),
     'Dance Club': useRef<HTMLDivElement>(null),
   };
@@ -29,18 +29,28 @@ const Club: FC = () => {
 
   return (
     <div className="container mx-auto py-8 px-4">
-      <h1 className="text-3xl font-bold text-center mb-8 text-dwd-primary">Our Clubs</h1>
+      <h1 className="text-3xl font-bold text-center mb-8 text-dwd-primary">
+        Our Clubs
+      </h1>
 
       <div className="bg-gray-200 p-8 rounded-lg mb-8 relative">
         <div className="top-4 left-4">
           <button
-            className={`px-4 py-2 rounded-lg mr-4 ${showTechnicalClubs ? ' hover:border-black bg-dwd-primary text-white hover:text-sky-600' : 'bg-dwd-secondary1 hover:text-white'}`}
+            className={`px-4 py-2 rounded-lg mr-4 ${
+              showTechnicalClubs
+                ? ' hover:border-black bg-dwd-primary text-white hover:text-sky-600'
+                : 'bg-dwd-secondary1 hover:text-white'
+            }`}
             onClick={() => setShowTechnicalClubs(true)}
           >
             Technical Clubs
           </button>
           <button
-            className={`px-4 py-2 rounded-lg mt-4 md:mt-0 ${!showTechnicalClubs ? 'bg-dwd-primary hover:border-black text-white hover:text-sky-600' : 'bg-dwd-secondary1 hover:text-white'}`}
+            className={`px-4 py-2 rounded-lg mt-4 md:mt-0 ${
+              !showTechnicalClubs
+                ? 'bg-dwd-primary hover:border-black text-white hover:text-sky-600'
+                : 'bg-dwd-secondary1 hover:text-white'
+            }`}
             onClick={() => setShowTechnicalClubs(false)}
           >
             Non Technical Clubs
@@ -51,7 +61,12 @@ const Club: FC = () => {
           {showTechnicalClubs ? (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               {clubs
-                .filter((club: ClubData) => club.name !== 'Zeitgeist' && club.name !== 'Music Club' && club.name !== 'Dance Club')
+                .filter(
+                  (club: ClubData) =>
+                    club.name !== 'Zeitgeist' &&
+                    club.name !== 'Music Club' &&
+                    club.name !== 'Dance Club'
+                )
                 .map((club: ClubData) => (
                   <div
                     key={club.name}
@@ -65,7 +80,12 @@ const Club: FC = () => {
           ) : (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               {clubs
-                .filter((club: ClubData) => club.name === 'Zeitgeist' || club.name === 'Music Club' || club.name === 'Dance Club')
+                .filter(
+                  (club: ClubData) =>
+                    club.name === 'Zeitgeist' ||
+                    club.name === 'Music Club' ||
+                    club.name === 'Dance Club'
+                )
                 .map((club: ClubData) => (
                   <div
                     key={club.name}
@@ -83,7 +103,7 @@ const Club: FC = () => {
       <br />
       <br />
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 gap-y-12">
         {clubs.map((club: ClubData) => (
           <div key={club.name} ref={refs[club.name]}>
             <ClubCard
@@ -99,24 +119,30 @@ const Club: FC = () => {
   );
 };
 
-const ClubCard: FC<{ clubName: string; aboutText: string; members: { name: string; position: string }[]; imagePath: string }> = ({ clubName, aboutText, members, imagePath }) => {
+const ClubCard: FC<{
+  clubName: string;
+  aboutText: string;
+  members: { name: string; position: string }[];
+  imagePath: string;
+}> = ({ clubName, aboutText, members, imagePath }) => {
   const [showAbout, setShowAbout] = useState(true);
 
   return (
-    <div className="relative max-w-sm mx-auto bg-gray-200 p-4 rounded-md shadow-md mb-8 hover:border-dwd-primary border-2 border-transparent group">
-      <div className="absolute -top-8 left-1/2 transform -translate-x-1/2">
+    <div className="relative max-w-sm mx-auto h-full flex flex-col bg-gray-200 p-4 pt-0 rounded-md shadow-md mb-8 hover:border-dwd-primary border-2 border-transparent group">
+      <div className="w-full relative flex-none h-[110px]">
         <Image
           src={imagePath}
           alt={`${clubName} logo`}
           width={110}
           height={110}
-          className="rounded-full border-4 bg-dwd-primary group-hover:border-dwd-primary"
+          className="rounded-full border-4 absolute left-1/2 -translate-y-1/3 transform -translate-x-1/2 bg-dwd-primary group-hover:border-dwd-primary"
         />
       </div>
-      <br />
-      <div className="pt-12">
-        <h2 className="text-center text-2xl font-bold mb-4">{clubName}</h2>
-        {/* <div className="flex justify-center mb-4">
+
+      <div className="flex flex-1 flex-col justify-between">
+        <div className="flex flex-col">
+          <h2 className="text-center text-2xl font-bold mb-4">{clubName}</h2>
+          {/* <div className="flex justify-center mb-4">
           <button
             className={`px-4 py-2 cursor-pointer rounded-md mr-2 ${showAbout ? 'bg-dwd-primary hover:text-sky-600 text-white' : 'bg-dwd-secondary1 hover:text-white'}`}
             onClick={() => setShowAbout(true)}
@@ -130,37 +156,47 @@ const ClubCard: FC<{ clubName: string; aboutText: string; members: { name: strin
             MEMBERS
           </button>
         </div> */}
-        {showAbout ? (
-          <div className="text-center mb-4">{aboutText}</div>
-        ) : (
-          <div className="text-center mb-4">
-            <table className="min-w-full bg-dwd-secondary2">
-              <thead>
-                <tr>
-                  <th className="py-2">Name</th>
-                  <th className="py-2">Position</th>
-                </tr>
-              </thead>
-              <tbody>
-                {members.map((member, index) => (
-                  <tr key={index}>
-                    <td className="border px-4 py-2">{member.name}</td>
-                    <td className="border px-4 py-2">{member.position}</td>
+          {showAbout ? (
+            <div className="text-center mb-4">{aboutText}</div>
+          ) : (
+            <div className="text-center mb-4">
+              <table className="min-w-full bg-dwd-secondary2">
+                <thead>
+                  <tr>
+                    <th className="py-2">Name</th>
+                    <th className="py-2">Position</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        )}
-        <br />
+                </thead>
+                <tbody>
+                  {members.map((member, index) => (
+                    <tr key={index}>
+                      <td className="border px-4 py-2">{member.name}</td>
+                      <td className="border px-4 py-2">{member.position}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+
         <div className="flex justify-center space-x-4">
-          <a href="https://www.instagram.com" className="flex gap-2 hover:text-pink-600 hover:duration-800 hover:animate-ping">
+          <a
+            href="https://www.instagram.com"
+            className="flex gap-2 hover:text-pink-600 hover:duration-800 hover:animate-ping"
+          >
             <Instagram size="2rem" />
           </a>
-          <a href="https://www.linkedin.com/in/iiitdharwad/" className="flex gap-2 hover:text-blue-800 hover:duration-800 hover:animate-pulse">
+          <a
+            href="https://www.linkedin.com/in/iiitdharwad/"
+            className="flex gap-2 hover:text-blue-800 hover:duration-800 hover:animate-pulse"
+          >
             <LinkedinIcon size="2rem" />
           </a>
-          <a href="https://www.gmail.com" className="flex gap-2 hover:text-red-700 hover:duration-800 hover:animate-pulse">
+          <a
+            href="https://www.gmail.com"
+            className="flex gap-2 hover:text-red-700 hover:duration-800 hover:animate-pulse"
+          >
             <Mail size="2rem" />
           </a>
         </div>
