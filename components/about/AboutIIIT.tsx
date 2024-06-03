18d672e1a7d0bfe85bb183384b4692d1512a237d
Nikhil Karthik <nikhilkarthik241103@gmail.com>
2024-06-03 13:16:06 +0530

    About page fixed

--- components/about/AboutIIIT.tsx
+++ components/about/AboutIIIT.tsx
@@ -3,11 +3,11 @@ interface AboutIIITProps {
 }
 const AboutIIIT = ({ aboutText }: AboutIIITProps) => {
   return (
-    <div className="w-full p-8 flex flex-col items-center bg-dwd-secondary2/40">
+    <div className="w-full p-8 flex md:px-16 lg:px-24 2xl:px-44 flex-col items-center bg-dwd-secondary2/40">
       <span className="font-bold mb-4 text-3xl md:text-4xl text-dwd-primary">
         About IIIT Dharwad
       </span>
-      <p className="md:text-lg">{aboutText}</p>
+      <p className="text-justify">{aboutText}</p>
     </div>
   );
 };
