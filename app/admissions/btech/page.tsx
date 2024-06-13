47981bec782d8a171ec312ca3f45dcb2c6fee61b
pratik <pratikpakhale20@gmail.com>
2024-06-13 15:15:52 +0530

    fixed a typo

--- app/admissions/btech/page.tsx
+++ app/admissions/btech/page.tsx
@@ -22,8 +22,7 @@ export default function Page() {
         className='mx-auto my-2 text-dwd-primary  block hover:underline underline-offset-2 after:-translate-y-1/3 after:absolute relative after:content-[url("/icons/linkIcon.svg")] w-fit self-center'
         href={'https://forms.gle/XYBoGw9xJQZ7gkzYA'}
       >
-        IIIT Dharwad Undergraduate Admission 2024 Form IIIT Dharwad
-        Undergraduate Admission 2024 Queries Form
+        IIT Dharwad Undergraduate Admission 2024 Queries Form
       </a>
       <Image
         alt="supernumerary admission banner"
