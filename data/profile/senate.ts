09d715eb2d301aca982b4ef28244e5bf4d46361f
palepen <122384892+palepen@users.noreply.github.com>
2024-06-18 20:52:49 +0530

    fixed bug

--- data/profile/senate.ts
+++ data/profile/senate.ts
@@ -16,32 +16,32 @@ export default [
     profiles: [
       {
         content: ['Professor, IIT Madras'],
-        imageURL: ['/images/madhu_mutyam.png'],
+        imageURL: '/images/madhu_mutyam.png',
         title: 'Prof. Madhu Mutyam',
       },
       {
         content: ['Professor, IIT Hyderabad'],
-        imageURL: ['/images/ch_subrahmanyam.png'],
+        imageURL: '/images/ch_subrahmanyam.png',
         title: 'Prof. Ch. Subrahmanyam',
       },
       {
         content: ['Professor, IIT Dharwad'],
-        imageURL: ['/images/dhiraj_v_patil.png'],
+        imageURL: '/images/dhiraj_v_patil.png',
         title: 'Prof. Dhiraj V. Patil',
       },
       {
         content: ['Professor, IIIT Hyderabad'],
-        imageURL: ['/images/vasudeva_verma.png'],
+        imageURL: '/images/vasudeva_verma.png',
         title: 'Prof. Vasudeva Verma',
       },
       {
         content: ['McAfee Bengaluru'],
-        imageURL: ['/images/dattatraya_kulkarni.png'],
+        imageURL: '/images/dattatraya_kulkarni.png',
         title: 'Dr. Dattatraya Kulkarni',
       },
       {
         content: ['Intel Bengaluru'],
-        imageURL: ['/images/sumeet_verma.png'],
+        imageURL: '/images/sumeet_verma.png',
         title: 'Shri Sumeet Verma',
       },
       {
