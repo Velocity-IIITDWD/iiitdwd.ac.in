a8d7f8ff9f3d84512f0e2550442cae6556993a32
Harsh Soni <52278293+harshsoni-harsh@users.noreply.github.com>
2024-06-19 21:29:01 +0530

    removed prefetching from next/link in 2 pages

--- app/annual_reports/MobileButton.tsx
+++ app/annual_reports/MobileButton.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 
 export default function Button(report: issueStructure) {
   return (
-    <Link href={report.issueUrl} className='border-2 bg-dwd-secondary2/25 border-dwd-secondary2 p-2 gap-2 w-40 flex flex-col justify-center rounded'>
+    <Link prefetch={false} href={report.issueUrl} className='border-2 bg-dwd-secondary2/25 border-dwd-secondary2 p-2 gap-2 w-40 flex flex-col justify-center rounded'>
       <Image alt="annual report cover image" loading='lazy' src={report.previewImg} height={200} width={200} className='w-fit h-fit rounded-t-md bg-white' />
       <p className='bg-dwd-primary px-4 py-2 rounded text-white w-fit self-center text-center'>
         {report.displayText}
