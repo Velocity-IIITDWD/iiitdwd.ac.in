4b34af065425638a3a2509d72a45598057d6817b
github-build <github-build@github.com>
2024-06-20 13:41:46 +0000

    Automated build commit

---
+++ out/_next/static/chunks/app/(profile)/senate/page-c65e491faf65761d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7982,6620,8806,7997,3292,3007,9846,8049,944,8698,8929],{5671:function(n,e,u){Promise.resolve().then(u.t.bind(u,8173,23))}},function(n){n.O(0,[8173,2971,7023,1744],function(){return n(n.s=5671)}),_N_E=n.O()}]);
