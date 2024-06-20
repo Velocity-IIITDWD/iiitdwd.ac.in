57a2a56fad67735c380bfb70640cfafbe5750b4e
github-build <github-build@github.com>
2024-06-20 14:21:25 +0000

    Automated build commit

--- out/_next/static/chunks/app/(profile)/board_of_governors/former/page-6dd3dc209c723588.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6620,8806,7997,3292,3007,9846,8049,944,7982,8698,8929],{5671:function(n,e,u){Promise.resolve().then(u.t.bind(u,8173,23))}},function(n){n.O(0,[8173,2971,7023,1744],function(){return n(n.s=5671)}),_N_E=n.O()}]);
