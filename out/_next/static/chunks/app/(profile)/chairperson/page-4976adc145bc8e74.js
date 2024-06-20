55acc5ca61dba1db880f4094034fc454d82bdc85
github-build <github-build@github.com>
2024-06-20 13:22:41 +0000

    Automated build commit

---
+++ out/_next/static/chunks/app/(profile)/chairperson/page-4976adc145bc8e74.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7997,6620,8806,3292,3007,9846,8049,7982,944,8698,8929],{5671:function(n,e,u){Promise.resolve().then(u.t.bind(u,8173,23))}},function(n){n.O(0,[8173,2971,7023,1744],function(){return n(n.s=5671)}),_N_E=n.O()}]);
