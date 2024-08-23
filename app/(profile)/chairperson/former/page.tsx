a0d503fc4cb124112fb81e40e2a0db3b52e9386e
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2024-08-23 12:50:56 +0530

    feat: Add former chairperson's name

--- app/(profile)/chairperson/former/page.tsx
+++ app/(profile)/chairperson/former/page.tsx
@@ -24,18 +24,21 @@ export default function FormerChairpersonPage() {
             />``
           </div>
 
-          <div className='text-dwd-primary p-2 text-[15px] text-center'>
-            As the Chairperson of the Board of Governors of IIIT Dharwad, I would like to share a slice of wisdom
-            with the young generation around us: Vision without action is merely a dream; action without vision is
-            merely passing time; but vision and action together can change the world. We can give our children only
-            two things in life which are essential – strong roots and powerful wings. Afterwards, they may fly anywhere
-            and live independently; of all the luxuries in life, the greatest one is getting freedom of the right 
-            kind. To come up in life, the young generation requires talent, hard work, drive, and ability to communicate
-            genuinely with people. With my experience in life, I feel that having good relationships, compassion,
-            and peace of mind is much more important than achievements, awards, degrees, or money. Hence, we should
-            always have a definite aim in life which we must try to achieve while being of help to others. I wish
-            IIIT Dharwad great success in shaping young minds into professionals with the best of skills coupled with
-            the highest of values.” - Smt. Sudha Murty, Chairperson of Board
+          <div className='flex flex-col gap-2 text-dwd-primary p-2 text-[15px] items-center xl:items-start'>
+            <div className='text-lg font-bold'>Smt. Sudha Murty</div>
+            <div className='text-[15px] text-center xl:text-left'>
+              As the Chairperson of the Board of Governors of IIIT Dharwad, I would like to share a slice of wisdom
+              with the young generation around us: Vision without action is merely a dream; action without vision is
+              merely passing time; but vision and action together can change the world. We can give our children only
+              two things in life which are essential – strong roots and powerful wings. Afterwards, they may fly anywhere
+              and live independently; of all the luxuries in life, the greatest one is getting freedom of the right 
+              kind. To come up in life, the young generation requires talent, hard work, drive, and ability to communicate
+              genuinely with people. With my experience in life, I feel that having good relationships, compassion,
+              and peace of mind is much more important than achievements, awards, degrees, or money. Hence, we should
+              always have a definite aim in life which we must try to achieve while being of help to others. I wish
+              IIIT Dharwad great success in shaping young minds into professionals with the best of skills coupled with
+              the highest of values.” - Smt. Sudha Murty, Chairperson of Board
+            </div>
           </div>
         </div>
       </div>
