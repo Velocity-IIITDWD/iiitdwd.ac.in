f661471bc84f3a8b51815786268e436a29d6746a
Vishesh-Dwivedi-Git <visheshdwivedi225544@gmail.com>
2024-09-10 22:21:23 +0530

    removed old phd&mtech data

--- app/admissions/phd/page.tsx
+++ app/admissions/phd/page.tsx
@@ -176,7 +176,7 @@ export default function Page() {
                   </tbody>
                 </table>
               </div>
-            </div> */}
+            </div> 
             <div className="my-4">
               Areas of Research interests include Computer Science and
               Engineering, Data Science and Artificial Intelligence, Electronics
@@ -188,7 +188,7 @@ export default function Page() {
               Things, Machine Learning, Deep Learning, Optimization, Signal
               Processing, Soft Computing, Social Networks, Physics, Quantum
               Computing, Mathematics and Statistics
-            </div>
+            </div>*/}
           </div>
         </div>
       </div>
