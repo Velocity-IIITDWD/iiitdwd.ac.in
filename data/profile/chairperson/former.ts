0a878365a5e8484c35810cd7f2d3b7847854e712
palepen <122384892+palepen@users.noreply.github.com>
2024-09-11 11:19:57 +0530

    fix: removed Annoucement on Dr. Prakash Pawar Research Paper

--- data/profile/chairperson/former.ts
+++ data/profile/chairperson/former.ts
@@ -5,7 +5,7 @@ export default [
     title: '',
     profiles: [
       {
-        content: [''],
+        content: ['As the Chairperson of the Board of Governors of IIIT Dharwad, I would like to share a slice of wisdom with the young generation around us: Vision without action is merely a dream; action without vision is merely passing time; but vision and action together can change the world. We can give our children only two things in life which are essential – strong roots and powerful wings. Afterwards, they may fly anywhere and live independently; of all the luxuries in life, the greatest one is getting freedom of the right kind. To come up in life, the young generation requires talent, hard work, drive, and ability to communicate genuinely with people. With my experience in life, I feel that having good relationships, compassion, and peace of mind is much more important than achievements, awards, degrees, or money. Hence, we should always have a definite aim in life which we must try to achieve while being of help to others. I wish IIIT Dharwad great success in shaping young minds into professionals with the best of skills coupled with the highest of values.'],
         imageURL: '/images/sudha_murty.jpg',
         title: 'Smt. Sudha Murty',
       },
