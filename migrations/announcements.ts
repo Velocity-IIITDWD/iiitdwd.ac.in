262c9415c3c377aeee9ae6f4bb8b70a75bea26c3
Harsh <52278293+harshsoni-harsh@users.noreply.github.com>
2025-01-23 21:09:58 +0530

    fix: update migrations logic

--- migrations/announcements.ts
+++ migrations/announcements.ts
@@ -2,27 +2,21 @@ import { client } from './utils/sanity.ts';
 import { announcements } from '../data/announcements.ts';
 
 export async function migrateAnnouncements() {
-  try {
-    const transaction = client.transaction();
+  const transaction = client.transaction();
 
-    for (const announcement of announcements) {
-      transaction.create({
-        _type: 'announcement',
-        link: announcement.link,
-        text: announcement.text,
-        new: announcement.new,
-        year: announcement.year,
-        month: announcement.month,
-      });
-    }
-
-    const results = await transaction.commit();
-    console.log(
-      `Created ${results.results.length} announcements in a transaction.`
-    );
-  } catch (error) {
-    console.error('Error migrating announcements:', error);
+  for (const announcement of announcements) {
+    transaction.create({
+      _type: 'announcement',
+      link: announcement.link,
+      text: announcement.text,
+      new: announcement.new,
+      year: announcement.year,
+      month: announcement.month,
+    });
   }
-}
 
-migrateAnnouncements();
+  const results = await transaction.commit();
+  console.log(
+    `Created ${results.results.length} announcements in a transaction.`
+  );
+}
